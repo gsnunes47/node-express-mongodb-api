c2203c274c55b89a478aa25df3e54f1447501159
gsnunes47 <gustavompn06@gmail.com>
2025-02-03 20:46:19 -0300

    criação de .env

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 const port = 3000;
-mongoose.connect(
-  "mongodb+srv://gustavompn06:871lAGfiMow0lKaH@starwars-api.7uofv.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api"
-);
+mongoose.connect(process.env.DB_URI);
 
 // models
 
@@ -23,15 +22,15 @@ const Film = mongoose.model("Film", {
 
 //create
 app.post("/", async (req, res) => {
-    const film = new Film({
-        title: req.body.title,
-        description: req.body.description,
-        image_url: req.body.image_url,
-        trailer_url: req.body.trailer_url,
-    });
-    
-    await film.save();
-    return res.send(film); //
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+
+  await film.save();
+  return res.send(film); //
 });
 
 //read
@@ -40,21 +39,27 @@ app.get("/", async (req, res) => {
   return res.send(films);
 });
 
+//update
 app.put("/:id", async (req, res) => {
-  const film = await Film.findByIdAndUpdate(req.params.id, {
-    title: req.body.title,
-    description: req.body.description,
-    image_url: req.body.image_url,
-    trailer_url: req.body.trailer_url,
-  }, {
-    new: true
-  });
+  const film = await Film.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      description: req.body.description,
+      image_url: req.body.image_url,
+      trailer_url: req.body.trailer_url,
+    },
+    {
+      new: true,
+    }
+  );
 
-//   const updateFilm = await Film.findById((id = req.params.id));
+  //   const updateFilm = await Film.findById((id = req.params.id));
 
   return res.send(updateFilm);
 });
 
+//delete
 app.delete("/:id", async (req, res) => {
   const film = await Film.findByIdAndDelete(req.params.id);
   return res.send(film);
